ea0e90acdf8b5607be269b77d6b32627286cf9a2
acehinnnqru <acehinnnqru@gmail.com>
2024-12-15 11:27:18 +0800

    feat: flake udpate

--- go_1_18/flake.nix
+++ go_1_18/flake.nix
@@ -3,19 +3,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     nixpkgs_with_go118.url = "github:NixOS/nixpkgs/517501bcf14ae6ec47efd6a17dda0ca8e6d866f9";
-    nixpkgs_with_gopls.url = "github:NixOS/nixpkgs/9a9dae8f6319600fa9aebde37f340975cab4b8c0";
   };
 
-  outputs = { self, nixpkgs, utils, nixpkgs_with_go118, nixpkgs_with_gopls }:
+  outputs = { self, nixpkgs, utils, nixpkgs_with_go118 }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
         pkgs_with_go118 = import nixpkgs_with_go118 {
-          inherit system;
-        };
-        pkgs_with_gopls = import nixpkgs_with_gopls {
           inherit system;
         };
       in
@@ -40,10 +36,6 @@
               with pkgs_with_go118; [
                 # go contains go and gofmt
                 go_1_18
-              ]
-            )
-            ++ (
-              with pkgs_with_gopls; [
                 gopls
               ]
             )
