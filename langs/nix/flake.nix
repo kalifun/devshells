68fe36e978eff7f0b48c9c987e002227dff4eaee
acehinnnqru <acehinnnqru@gmail.com>
2025-01-01 15:40:18 +0800

    feat: add langs/nix flake

--- langs/nix/flake.nix
+++ langs/nix/flake.nix
@@ -2,32 +2,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    base.url = "github:acehinnnqru/devshells?dir=base";
+    baseshell.url = "github:acehinnnqru/devshells?dir=base";
   };
 
-  outputs = { self, nixpkgs, utils, base }:
+  outputs = { self, nixpkgs, utils, baseshell }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+        devShell = with pkgs; mkShell {
+          inputsFrom = [
+            baseshell.devShells.${system}.default
+          ];
+          packages = [
+            nil
+          ];
+        };
       in
       {
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            base.devShells.${system}.default
-
-            nil
-          ] ++ lib.optionals stdenv.isDarwin (with darwin;
-            with apple_sdk.frameworks; [
-              libiconv
-              libresolv
-              Libsystem
-              SystemConfiguration
-              Security
-              CoreFoundation
-            ]);
-        };
+        devShells.default = devShell;
       }
     );
 }
