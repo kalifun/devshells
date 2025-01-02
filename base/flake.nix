653e8a0352944da6c8ca2ffb2d6b126357c93a3a
acehinnnqru <acehinnnqru@gmail.com>
2025-01-02 09:57:03 +0800

    chore: fmt base/flake.nix

--- base/flake.nix
+++ base/flake.nix
@@ -4,37 +4,42 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
         };
-        devShell = with pkgs; mkShell {
-          packages = [
-            ## basic pkgs
-            tree-sitter
+        devShell = with pkgs;
+          mkShell {
+            packages =
+              [
+                ## basic pkgs
+                tree-sitter
 
-            ## basic lsp
-            # lsp for yaml
-            yaml-language-server
-            # lsp for markdown
-            marksman
-            # lsp for json, markdown, css, html, eslint
-            vscode-langservers-extracted
-
-          ] ++ lib.optionals stdenv.isDarwin (with darwin;
-            with apple_sdk.frameworks; [
-              libiconv
-              libresolv
-              Libsystem
-              SystemConfiguration
-              Security
-              CoreFoundation
-            ]);
-        };
-      in
-      {
+                ## basic lsp
+                # lsp for yaml
+                yaml-language-server
+                # lsp for markdown
+                marksman
+                # lsp for json, markdown, css, html, eslint
+                vscode-langservers-extracted
+              ]
+              ++ lib.optionals stdenv.isDarwin (with darwin;
+                with apple_sdk.frameworks; [
+                  libiconv
+                  libresolv
+                  Libsystem
+                  SystemConfiguration
+                  Security
+                  CoreFoundation
+                ]);
+          };
+      in {
         devShells.default = devShell;
       }
     );
