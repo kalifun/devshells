3295317b9af6e012b518030761d5813bf75a1976
acehinnnqru <acehinnnqru@gmail.com>
2025-01-02 11:57:37 +0800

    feat: add lua

---
+++ langs/lua/flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    baseshell.url = "github:acehinnnqru/devshells?dir=base";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    baseshell,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = with pkgs;
+          mkShell {
+            inputsFrom = [
+              baseshell.devShells.${system}.default
+            ];
+
+            packages = [
+              lua
+              stylua
+            ];
+
+            AU_LANG_LUA = "1";
+          };
+      }
+    );
+}
