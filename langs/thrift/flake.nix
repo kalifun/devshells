53662a7cd169f157fc90a9481ece0f2ad1f8a021
acehinnnqru <acehinnnqru@gmail.com>
2025-01-04 01:47:52 +0800

    feat: add thrift lang

---
+++ langs/thrift/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    baseshell.url = "github:acehinnnqru/devshells?dir=base";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    baseshell,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        devShell = with pkgs;
+          mkShell {
+            inputsFrom = [
+              baseshell.devShells.${system}.default
+            ];
+            packages = [
+              thrift-ls
+            ];
+
+            AU_LANG_THRIFT = "1";
+          };
+      in {
+        devShells.default = devShell;
+      }
+    );
+}
