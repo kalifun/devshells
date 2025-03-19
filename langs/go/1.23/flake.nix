1b871942f19fdb070029e38e8e427252b684d4f2
acehinnnqru <acehinnnqru@gmail.com>
2025-03-19 18:25:03 +0800

    feat: upgrade go latest to 1.24; add 1.23 package

---
+++ langs/go/1.23/flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    baseshell.url = "github:acehinnnqru/devshells?dir=base";
+    nixpkgs_with_go123.url = "github:NixOS/nixpkgs/a343533bccc62400e8a9560423486a3b6c11a23b";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    baseshell,
+    nixpkgs_with_go123,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        pkgs_with_go120 = import nixpkgs_with_go123 {
+          inherit system;
+        };
+        devShell = with pkgs;
+          mkShell {
+            inputsFrom = [
+              baseshell.devShells.${system}.default
+            ];
+
+            packages =
+              [
+                libiconv
+                gcc
+
+                # go contains go and gofmt
+                go_1_23
+
+                golangci-lint
+                gopls
+                gotools
+                gomodifytags
+              ]
+              ++ (
+                with pkgs_with_go120; [
+                  # go contains go and gofmt
+                  go_1_20
+                  gopls
+                ]
+              );
+
+            AU_LANG_GO = "1";
+          };
+      in {
+        devShells.default = devShell;
+      }
+    );
+}
