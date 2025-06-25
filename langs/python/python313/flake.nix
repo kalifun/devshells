8d594007e13fdfbb75e49c1399a17046c47c4171
acehinnnqru <acehinnnqru@gmail.com>
2025-06-25 13:35:23 +0800

    feat: add python313

---
+++ langs/python/python313/flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    baseshell.url = "github:acehinnnqru/devshells?dir=base";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    baseshell,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        devShell = with pkgs;
+          mkShell {
+            inputsFrom = [
+              baseshell.devShells.${system}.default
+            ];
+
+            packages = [
+              python313
+
+              uv
+              ruff
+              ty
+            ];
+
+            UV_PYTHON_DOWNLOADS = "never";
+          };
+      in {
+        devShells.default = devShell;
+      }
+    );
+}
