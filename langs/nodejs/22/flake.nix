e81cf9a3385d9b4485eebccd4498336465cc3180
acehinnnqru <acehinnnqru@gmail.com>
2025-07-24 20:29:21 +0800

    feat: add nodejs 22

---
+++ langs/nodejs/22/flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    baseshell.url = "github:acehinnnqru/devshells?dir=base";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    baseshell,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        devShell = with pkgs;
+          mkShell {
+            inputsFrom = [
+              baseshell.devShells.${system}.default
+            ];
+
+            packages = [
+              libiconv
+              gcc
+
+              # basic
+              nodejs_22
+              yarn
+              pnpm
+
+              ## lsp
+              # lsp for ts
+              typescript-language-server
+              # lsp for json, markdown, css, html, eslint
+              vscode-langservers-extracted
+
+              # formatter
+              prettierd
+            ];
+
+            AU_LANG_TS = 1;
+            AU_LANG_CSS = 1;
+            AU_LANG_HTML = 1;
+          };
+      in {
+        devShells.default = devShell;
+      }
+    );
+}
