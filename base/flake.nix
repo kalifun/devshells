788890adc6e42dfc3a9b836ab0270da2aeed9937
acehinnnqru <acehinnnqru@gmail.com>
2025-08-30 14:55:35 +0800

    feat: update

--- base/flake.nix
+++ base/flake.nix
@@ -16,18 +16,28 @@
         };
         devShell = with pkgs;
           mkShell {
-            packages = [
-              ## basic pkgs
-              tree-sitter
+            packages =
+              [
+                ## basic pkgs
+                tree-sitter
 
-              ## basic lsp
-              # lsp for yaml
-              yaml-language-server
-              # lsp for markdown
-              marksman
-              # lsp for json, markdown, css, html, eslint
-              vscode-langservers-extracted
-            ];
+                ## basic lsp
+                # lsp for yaml
+                yaml-language-server
+                # lsp for markdown
+                marksman
+                # lsp for json, markdown, css, html, eslint
+                vscode-langservers-extracted
+              ]
+              ++ lib.optionals stdenv.isDarwin (with darwin;
+                with apple-sdk.frameworks; [
+                  libiconv
+                  libresolv
+                  Libsystem
+                  SystemConfiguration
+                  Security
+                  CoreFoundation
+                ]);
           };
       in {
         devShells.default = devShell;
