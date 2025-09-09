a1875607bc1867c052fba6870cc0b4137f178ebd
acehinnnqru <acehinnnqru@gmail.com>
2025-09-09 15:56:14 +0800

    fix(go/1.21): use fixed nixpkgs for go 1.21

--- langs/go/1.21/flake.nix
+++ langs/go/1.21/flake.nix
@@ -7,6 +7,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.utils.follows = "utils";
     };
+    nixpkgs_with_go121.url = "github:NixOS/nixpkgs/5ed627539ac84809c78b2dd6d26a5cebeb5ae269";
   };
 
   outputs = {
@@ -14,10 +15,14 @@
     nixpkgs,
     utils,
     baseshell,
+    nixpkgs_with_go121,
   }:
     utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
+          inherit system;
+        };
+        pkgs_with_go121 = import nixpkgs_with_go121 {
           inherit system;
         };
       in {
@@ -27,18 +32,22 @@
               baseshell.devShells.${system}.default
             ];
 
-            packages = [
-              libiconv
-              gcc
+            packages =
+              [
+                libiconv
+                gcc
 
-              # go contains go and gofmt
-              go_1_21
-
-              golangci-lint
-              gopls
-              gotools
-              gomodifytags
-            ];
+                # go contains go and gofmt
+                golangci-lint
+                gotools
+                gomodifytags
+              ]
+              ++ (
+                with pkgs_with_go121; [
+                  go_1_21
+                  gopls
+                ]
+              );
 
             AU_LANG_GO = "1";
           };
