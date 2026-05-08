27921607529b92810c46e59f2667c56df5439bc3
acehinnnqru <acehinnnqru@gmail.com>
2026-05-08 09:53:28 +0800

    feat(rust): rewrite rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -6,26 +6,49 @@
       url = "github:acehinnnqru/devshells";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     utils,
     devshells,
+    fenix,
     ...
   }:
     utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [fenix.overlays.default];
+        };
+
+        # wasmToolchain = with fenix.packages.${system};
+        #   combine [
+        #     complete.toolchain
+        #     targets.wasm32-unknown-unknown.latest.rust-std
+        #   ];
       in {
         devShells.default = pkgs.mkShell {
-          inputsFrom = [
-            devshells.devShells.${system}."rust-stable"
+          nativeBuildInputs = [
+            # fenix.packages.${system}.stable.toolchain
+            # fenix.packages.${system}.complete.toolchain
+            devshells.devShells.${system}."nix"
           ];
           packages = with pkgs; [
             # install the packages you need
             # ripgrep
+            cargo-nextest
+            taplo
+
+            # rust-analyzer-nightly
+            # rust-analyzer
           ];
+
+          RUST_BACKTRACE = 1;
 
           shellHook = ''
             echo "enter custom devshells"
